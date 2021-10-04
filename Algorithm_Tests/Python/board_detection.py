ffc91a1c00496167a2917de940e5717a0ba42861
BergTim <timon@bergmann.ch>
2021-10-04 19:59:28 +0200

    board_detection.py edit

--- Algorithm_Tests/Python/board_detection.py
+++ Algorithm_Tests/Python/board_detection.py
@@ -17,37 +17,44 @@ output = img.copy()
 #make grayscale image
 img_gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
+#morphological operations
 kernel = np.ones((5,5),np.uint8)
 erosion = cv2.erode(img_gray,kernel,iterations = 1)
 
+#Take contours 
 contours = img_gray - erosion
 cv2.imshow('erosion subtraction', contours)
-
-
-
 
 cv2.imshow('image normal', img) 
 cv2.imshow('grayscale', img_gray) 
 
 #line detection with grayscale image-------------
 kernel_size =5
-blur_gray = cv2.GaussianBlur(img_gray,(kernel_size, kernel_size),0)
-
-#morphological operations
-kernel = np.ones((5,5),np.uint8)
-erosion = cv2.erode(img,kernel,iterations = 1)
-
+blur_gray = cv2.GaussianBlur(contours,(kernel_size, kernel_size),0)
 
 #canny
-low_threshold = 80
-high_threshold = 250
+low_threshold = 10
+high_threshold = 220
 edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
 cv2.imshow('canny', edges)
  
 #Hough circles(detect circles on image)
-circles = cv2.HoughCircles(edges,cv2.HOUGH_GRADIENT, 1.2, 100)
+#gray = cv2.medianBlur(gray, 5)
+
+circles = cv2.HoughCircles(edges,cv2.HOUGH_GRADIENT, 1.2, 500)
 cv2.imshow('Hough Circles', edges) 
 
+if circles is not None:
+    circles = np.uint16(np.around(circles))
+    for i in circles[0, :]:
+        center = (i[0], i[1])
+        # circle center
+        cv2.circle(img, center, 1, (0, 100, 100), 3)
+        # circle outline
+        radius = i[2]
+        cv2.circle(img, center, radius, (255, 0, 255), 3)
+            
+cv2.imshow("detected circles", img)
 # show the output image
 cv2.imshow("output", np.hstack([img, output]))
 #cv2.waitKey(0)        
