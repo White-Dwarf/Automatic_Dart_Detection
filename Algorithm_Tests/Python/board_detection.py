ec6b2af02e8125eeb74ff1b30a3f099b59e355ff
BergTim <timon@bergmann.ch>
2021-10-22 23:48:51 +0200

    edit board detection script
    -Separating color space integrated,
    -sorting contours, rectangle boxing edit

--- Algorithm_Tests/Python/board_detection.py
+++ Algorithm_Tests/Python/board_detection.py
@@ -12,8 +12,17 @@ import cv2
 img = cv2.imread('./../../images/Dartboard/Dartboard_0Darts_Bright.png') 
 hd_img = cv2.imread('./../../images/Dart_Board_Color.png') 
 
-cv2.imshow('image normal', img) 
-cv2.imshow('HD image normal', hd_img) 
+#cv2.imshow('image normal', img) 
+#cv2.imshow('HD image normal', hd_img) 
+
+#color separation on image
+img_hsv = cv2.cvtColor(hd_img, cv2.COLOR_BGR2HSV)
+mask = cv2.inRange(hd_img,(40,40,40),(70,255,255))
+hd_img_red = cv2.bitwise_and(hd_img, hd_img, mask = mask)
+
+cv2.imshow('color mask', mask) 
+cv2.imshow('extracted red', hd_img_red) 
+
 
 #make copy to later output detections on rgb image
 output = img.copy()
@@ -58,6 +67,28 @@ cv2.imshow('drawn contours', output)
 hd_image, hd_contours, hd_hierachy =cv2.findContours(hd_thresh,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
 cv2.drawContours(hd_img, hd_contours, contourIdx=-1, color=(0, 255, 0), thickness=1, lineType=cv2.LINE_AA)
 cv2.imshow('HD drawn contours', hd_img)
+
+
+
+
+####Sorting contours --> draw bounding boxes over contour
+contoursSorted_hd = sorted(hd_contours, key=cv2.contourArea, reverse=True)
+
+x,y,w,h = cv2.boundingRect(hd_contours[-1])
+cv2.rectangle(hd_img,(x,y),(x+w,y+h),(0,255,0),2)
+
+
+rect = cv2.minAreaRect(hd_contours[-1])
+box = cv2.boxPoints(rect)
+box = np.int0(box)
+cv2.drawContours(hd_img,[box],0,(0,0,255),2)
+
+
+cv2.imshow('Box', hd_img)
+
+
+
+
 
 
 # show the output image
